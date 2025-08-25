2dc6bc1a3f39f0303b6ec500e449c5a79378d760
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-08-25 11:25:12 +0300

    Construct square class

--- src/Square.java
+++ src/Square.java
@@ -1,4 +1,41 @@
-public class Square {
-    // what is contain, position, is_empty, set_piece, get_piece.
+import java.awt.Color;
+import javax.swing.JLabel;
 
+public class Square extends JLabel {
+    // what is contain, position, is_empty, set_piece, get_piece, remove_piece.
+
+    private int row;
+    private int col;
+    String piece = null; // -> will modify to be class after making pieces class.
+
+    public Square(Color c, int row, int col) {
+        this.setBackground(c);
+        this.setOpaque(true);
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isEmpty() {
+        return piece == null;
+    }
+
+    public String getPiece() {
+        return piece;
+    }
+
+    public void setPiece(String spiece) {
+        piece = spiece;
+    }
+
+    public void removePiece() {
+        piece = null;
+    }
 }
