32b89143e0b74c9a02883c805b242094258fb466
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-08-26 16:17:24 +0300

    modify the square class to make with button

--- src/Square.java
+++ src/Square.java
@@ -1,26 +1,30 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JLabel;
 
-public class Square extends JLabel {
+public class Square extends JLabel implements ActionListener {
     // what is contain, position, is_empty, set_piece, get_piece, remove_piece.
 
-    private int row;
-    private int col;
     String piece = null; // -> will modify to be class after making pieces class.
+    JButton button;
 
-    public Square(Color c, int row, int col) {
+    public Square(Color c) {
+
+        button = new JButton();
+        button.setBounds(this.getBounds());
+
+        // to make the button with no animation
+        button.setFocusPainted(false);
+        button.setBorderPainted(false);
+        button.setContentAreaFilled(false);
+
+        this.add(button);
         this.setBackground(c);
         this.setOpaque(true);
-        this.row = row;
-        this.col = col;
-    }
 
-    public int getRow() {
-        return row;
-    }
-
-    public int getCol() {
-        return col;
     }
 
     public boolean isEmpty() {
@@ -37,5 +41,13 @@ public class Square extends JLabel {
 
     public void removePiece() {
         piece = null;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            // will be implemented
+        }
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
 }
