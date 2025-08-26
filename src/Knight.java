34ceb7e6e390550df7f7202de3f9bb6912494c37
EngAhmedgabr <ahmedmeagabr@gmail.com>
2025-08-26 15:55:04 +0300

    Update Knight.java

--- src/Knight.java
+++ src/Knight.java
@@ -1,20 +1,22 @@
 import javax.swing.*;
 
-public class King extends Piece{
-    boolean safe;
-    public King(String color, int row, int col, ImageIcon icon, boolean safe) {
-        super("King", color, row, col, icon);
-        this.safe=safe;
+public class Knight extends Piece {
+    public Knight(String color, int row, int col, ImageIcon icon) {
+        super("Knight", color, row, col, icon);
     }
 
     @Override
-    boolean isValidMove(int newRow, int newCol,Piece[][] board) { // i am using a board of pieces could be changed to be of squares
+    boolean isValidMove(int newRow, int newCol, Piece[][] board) {// i am using a board of pieces could be changed to be of squares
         // to know whether this part of the board contains a piece of the same or different color or if it is empty
-        int absRow = Math.abs(row-newRow);
-        int absCol = Math.abs(col-newCol);
         Piece target = board[newRow][newCol];
-        if((absCol<=1 && absRow<=1) && (!target.color.equals(color) || target==null)){
-            return true;
+        if(target==null || !target.color.equals(color)){
+            if((newRow==row+2 || newRow==row-2) && (newCol== col+1 || newCol==col-1)){
+                return true;
+            }
+            if((newRow==row+1 || newRow == row-1) && (newCol==col-2 || newCol == col+2)){
+                return true;
+            }
+            return false;
         }
         return false;
     }
@@ -23,5 +25,4 @@ public class King extends Piece{
     void move(int newRow, int newCol) {
         super.move(newRow, newCol);
     }
-
 }
