a200a8eae8342a6d75ad9b39abd99fa54d0ada1a
EngAhmedgabr <ahmedmeagabr@gmail.com>
2025-08-30 03:20:44 +0300

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
+
 public class Player implements Runnable {
     private String name;
     private String color;
     private int sec = 0;
     private int min = 0;
     private boolean turn = false;
+    private ArrayList<Piece> killedPieces;
 
     public Player(String name, String color) {
         this.name = name;
         this.color = color;
+        this.killedPieces = new ArrayList<>();
     }
 
     @Override
@@ -52,5 +56,17 @@ public class Player implements Runnable {
     public void reset() {
         sec = 0;
         min = 0;
+    }
+
+    public void setKilledPiece(Piece killedPiece){
+        killedPieces.add(killedPiece);
+    }
+
+    public ArrayList<Piece> getKilledPieces() {
+        return killedPieces;
+    }
+
+    public String getTime() { // i think this method will be easy when making the time panel and the player finished playing
+        return String.format("%02d:%02d", min, sec);
     }
 }
