3d3465832b8be385094c61136dc6c3e611870a8e
EngAhmedgabr <ahmedmeagabr@gmail.com>
2025-09-02 15:20:16 +0300

    Update Queen.java

--- src/Queen.java
+++ src/Queen.java
@@ -7,7 +7,7 @@ public class Queen extends Piece {
 
     @Override
     boolean isValidMove(int newRow, int newCol, Square[][] board) {
-        if (newRow < 1 || newRow > 8 || newCol < 1 || newCol > 8) {
+        if (newRow < 0 || newRow > 7 || newCol < 0 || newCol > 7) {
             return false;
         }
 
